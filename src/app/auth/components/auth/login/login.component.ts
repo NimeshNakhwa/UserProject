1e3c2b029f375842699de31b0c4fb758528a2236
NimeshNakhwa <nimeshnakhwa100@gmail.com>
2025-02-15 16:41:13 +0530

    FSD Day 3

--- src/app/auth/components/auth/login/login.component.ts
+++ src/app/auth/components/auth/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: false,
-  
+
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required,Validators.minLength(6)]],
+    });
 
+  }
 }
